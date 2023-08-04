ce37fa920a7c1719817013bf44085c6933991e8f
tcostello74 <tcostello74@yahoo.com>
2023-08-04 13:05:49 -0400

    Add DB

--- db.js
+++ db.js
@@ -1,2 +1,10 @@
 /** Database setup for BizTime. */
+const { Client } = require("pg");
 
+const client = new Client({
+  connectionString: "postgresql://postgres:password@localhost/biztime"
+});
+
+client.connect();
+
+module.exports = client;
